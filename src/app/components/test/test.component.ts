2d8a52ed90efcb10fa4c4ba4611497dded83097b
grizeus <zaytsewww@gmail.com>
2025-04-09 17:55:36 +0300

    practice with events and component communication

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'app-test',
-  imports: [],
-  templateUrl: './test.component.html',
-  styleUrl: './test.component.scss'
+  selector: "app-test",
+  imports: [CommonModule, FormsModule],
+  templateUrl: "./test.component.html",
+  styleUrl: "./test.component.scss",
 })
 export class TestComponent {
+  title: string = "This is interpolation";
+  firstName: string = "John";
+  lastName: string = "Doe";
 
+  private isEnabled: boolean = true;
+  isActive: boolean = true;
+  isDisabled: boolean = false;
+  isClickedState = false;
+  inputText: string = "";
+
+  @Input() childMessage: string = "";
+  @Output() messageFromChild = new EventEmitter<string>();
+
+  getIsEnabled() {
+    return this.isEnabled;
+  }
+
+  getFullName() {
+    return `My name is ${this.firstName} ${this.lastName}`;
+  }
+
+  toggleState() {
+    this.isClickedState = !this.isClickedState;
+  }
+
+  sendToParent() {
+    this.messageFromChild.emit("I'm your son");
+  }
 }
