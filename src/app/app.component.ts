60854cc50e2337fad0d02a1e5111aadb79628fd9
grizeus <32486511+grizeus@users.noreply.github.com>
2025-04-09 17:56:14 +0300

    practice with events and component communication (#2)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { TestComponent } from './components/test/test.component';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { TestComponent } from "./components/test/test.component";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   imports: [RouterOutlet, TestComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'interview-master';
+  title = "interview-master";
+  messageFromParent = "I'm your father";
+  messageFromChild = "";
+  getMessageFromChild(msg: string) {
+    this.messageFromChild = msg;
+  }
 }
