8068a24b5bede0d98935318667cd14e55e1937b7
grizeus <zaytsewww@gmail.com>
2025-04-10 15:37:02 +0300

    add structure directives exapmles

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -1,14 +1,27 @@
 import { CommonModule } from "@angular/common";
 import { Component, Input, Output, EventEmitter } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { HighlightDirective } from "../../directives/highlight.directive";
+
+type State = "active" | "paused" | "stopped" | null;
 
 @Component({
   selector: "app-test",
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, HighlightDirective],
   templateUrl: "./test.component.html",
   styleUrl: "./test.component.scss",
 })
 export class TestComponent {
+  items = [
+    { "id": 1, "name": "apple" },
+    { "id": 2, "name": "banana" },
+    { "id": 3, "name": "orange" },
+    { "id": 4, "name": "kiwi" },
+    { "id": 5, "name": "elderberry" },
+  ];
+
+  appState: State = "paused";
+
   title: string = "This is interpolation";
   firstName: string = "John";
   lastName: string = "Doe";
