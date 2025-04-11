a8d7dcb89bc94d947ac4e5558f36f89dbe1de616
grizeus <zaytsewww@gmail.com>
2025-04-11 16:20:23 +0300

    fix test case in app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, interview-master');
+    expect(compiled.querySelector('p')?.textContent).toContain('This is app!');
   });
 });
