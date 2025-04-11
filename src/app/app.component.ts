7a9289dcd294a7888e750b3ac926f15bd9318572
grizeus <32486511+grizeus@users.noreply.github.com>
2025-04-11 23:14:23 +0300

    add reactive form (#6)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { TestComponent } from "./components/test/test.component";
+import { NameEditorComponent } from "./components/name-editor/name-editor.component";
 
 @Component({
   selector: "app-root",
-  imports: [RouterOutlet, TestComponent],
+  imports: [RouterOutlet, TestComponent, NameEditorComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })
