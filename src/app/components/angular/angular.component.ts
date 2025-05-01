28e1084b7c5845a0f5ae3e0d1479a0fedcd4b0d4
grizeus <zaytsewww@gmail.com>
2025-05-02 00:27:50 +0300

    add lazy load, signal amd effect examples

--- src/app/components/angular/angular.component.ts
+++ src/app/components/angular/angular.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { Component, WritableSignal, signal, effect } from '@angular/core';
 
 @Component({
   selector: 'app-angular',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './angular.component.html',
   styleUrl: './angular.component.scss'
 })
 export class AngularComponent {
+  questions: WritableSignal<any[]> = signal(['apple', 'banana', 'orange']);
 
+  constructor() {
+    effect(() => {
+      console.log(`Updated: ${JSON.stringify(this.questions())}`);
+    });
+
+    const idx = setTimeout(() => this.addOnce(), 5000);
+  }
+
+  addOnce() {
+    this.questions.set([...this.questions(), 'mango']);
+  }
 }
