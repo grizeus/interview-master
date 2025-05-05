612432f9971479dfceaa60617213b8327042e6bb
grizeus <32486511+grizeus@users.noreply.github.com>
2025-05-06 00:25:01 +0300

    setup for deploy (#14)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,6 @@ import { RouterOutlet } from "@angular/router";
 
 import { TopMenuComponent } from "./components/top-menu/top-menu.component";
 import { LeftSideMenuComponent } from "./components/left-side-menu/left-side-menu.component";
-import { CategoryComponent } from "./components/category/category.component";
-import { DeleteConfirmationModalComponent } from "./components/delete-confirmation-modal/delete-confirmation-modal.component";
-import { GenerateAnswerModalComponent } from "./components/generate-answer-modal/generate-answer-modal.component";
-import { PreparationComponent } from "./components/preparation/preparation.component";
-import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
 import { UserPanelComponent } from "./components/user-panel/user-panel.component";
 
 @Component({
@@ -18,13 +13,8 @@ import { UserPanelComponent } from "./components/user-panel/user-panel.component
     RouterOutlet,
     MatSidenavModule,
     MatToolbarModule,
-    CategoryComponent,
-    DeleteConfirmationModalComponent,
-    GenerateAnswerModalComponent,
     LeftSideMenuComponent,
-    PreparationComponent,
     TopMenuComponent,
-    PageNotFoundComponent,
     UserPanelComponent,
   ],
   templateUrl: "./app.component.html",
